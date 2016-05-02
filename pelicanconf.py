3b24d98bd19cb53c3dfbad398f0688105b0bf21e
Luc Libralesso <libralesso.l@gmail.com>
2016-05-02 13:45:56 -0400

    add test files for new theme

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Luc Libralesso'
+SITENAME = u'Site personnel de Luc Libralesso'
+SITEURL = '' # PUT THE BASE URL HERE
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME='dest/kiwi/'
+
+KIWI_EDITO_IMAGE_LINK = SITEURL+'/images/avatar.png'
+KIWI_EDITO_IMAGE_ALT = 'Luc Libralesso'
+KIWI_EDITO_TITLE = 'A propos'
+KIWI_EDITO_TEXT = """
+Donec tempor ullamcorper vulputate. Vestibulum tristique mi arcu, in semper diam sagittis vitae. Donec aliquam enim in magna elementum, vel blandit quam suscipit.
+"""
+
+# FOR SEO
+BLOG_DESCRIPTION = "Page personnelle de Luc Libralesso"
+
+SOCIAL = [
+    ('twitter', 'librallu'),
+    ('github', 'librallu')
+]
+
+DEFAULT_PAGINATION = 10
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
---
+++ publishconf.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from pelicanconf import *
+
+SITEURL = 'http://librallu.github.io/'
